9fa702055a83a0dbf912f01f1aabcf27650f5dcc
ManojKGupta1301 <102594211+ManojKGupta1301@users.noreply.github.com>
2022-03-29 17:32:05 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import React ,{useState} from 'react';
+import Main from './components/MainPage/MainPage'
+import UploadButton from './components/UploadButton/UploadButton';
+
+function App() {
+
+   const [imageSrc, setImageSrc] = useState();
+   const [isOpen, setIsOpen] = useState(true)
+   function updateImageSrc (filepath) {
+     setImageSrc(filepath)
+   }
+   
+   function openHome() {
+      setIsOpen(false)
+   }
+   
+   if(isOpen) {return (
+      <div className="App">
+         <div >
+         <div className= 'logo'>
+         <h1 className = 'logo-text'>Meme Maker</h1>
+         <h3> Make a meme online and share it with your pals. </h3>
+         </div>
+         <div>
+         <UploadButton updateImageSrc = {updateImageSrc} openMain = {openHome}/> 
+         </div>
+      </div>
+      </div>   
+   )} 
+   else {
+      if(!imageSrc) return 'loading...'
+   }
+return (   
+   <Main imageSrc = {imageSrc}/>
+)
+}
+
+export default App;
